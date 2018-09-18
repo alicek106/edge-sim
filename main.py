8a939b24e927ef280a105242fdf400eaaa2f9429
alicek106 <alice_k106@naver.com>
2018-09-18 22:33:10 +0900

    EDIT main.py -> v0.10

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import ast
 import datetime, time
 from config import config
 import os
+from logger.Logger import logger
 
 ## Global variables
 current_milli_time = lambda: int(round(time.time() * 1000))
@@ -15,17 +16,25 @@ db = client['metrics']
 
 @app.route('/')
 def request_job():
-    priority = request.args.get('priority')
-    if priority == None:
+    start_time = time.time()
+    content = request.get_json(silent=True)
+    priority = content['priority']
+    cpu = content['cpu']
+    logger.info('Priority : %s, CPU : %s' % (priority, cpu))
+
+    if priority == 'None':
+        print('Priority is None.')
         priority = -1
         output = check_output('bash /STREAM/run.sh', shell=True, stderr=STDOUT)
     else:
         output = check_output('bash /STREAM/run.sh %s' % (priority), shell=True, stderr=STDOUT)
     output = output.decode('utf-8').replace('\n', '')
-    store_result(priority, output)
+    end_time = time.time()
+
+    store_result(priority, output, cpu, start_time, end_time)
     return json.dumps({'status': 'success'})
 
-def store_result(priority, result_name):
+def store_result(priority, result_name, cpu, start_time, end_time):
     data_dict = {}
     with open('/STREAM/%s' % result_name, 'r') as fp:
         # line from 5 ~ 16 will be stored in mongo table
@@ -42,8 +51,11 @@ def store_result(priority, result_name):
 
         data_dict['workload_name'] = config['workload_name']
         data_dict['host'] = config['host']
-        data_dict['date'] = datetime.datetime.utcnow()
+        data_dict['start_time'] = start_time
+        data_dict['end_time'] = end_time
+        # data_dict['date'] = datetime.datetime.utcnow()
         data_dict['priority'] = priority
+        data_dict['cpu'] = cpu
         db[config['host']].insert_one(data_dict)
 
     os.remove('/STREAM/%s' % result_name)
